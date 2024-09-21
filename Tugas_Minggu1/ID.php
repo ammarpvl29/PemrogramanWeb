5e4143db46da5ddc4a1a14739535826d3dff757e
ammarpvl29 <ammar.pavel@gmail.com>
2024-09-21 18:09:44 +0700

    meta nama jadi meta name

--- Tugas_Minggu1/ID.php
+++ Tugas_Minggu1/ID.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta nama="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tugas_Minggu1</title>
     <style>
         table {
